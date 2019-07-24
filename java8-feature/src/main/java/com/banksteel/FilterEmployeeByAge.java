0309fcbd67f32cc46930e950230dd80810674390
wukaifeng <1076208637@qq.com>
2019-07-24 13:07:59 +0800

    去除无用代码

--- java8-feature/src/main/java/com/banksteel/FilterEmployeeByAge.java
+++ java8-feature/src/main/java/com/banksteel/FilterEmployeeByAge.java
@@ -3,6 +3,11 @@ package com.banksteel;
 import com.banksteel.bean.Employee;
 import com.banksteel.interfaces.MyPredicate;
 
+/**
+ * 过滤年龄
+ * @author 吴凯丰
+ *
+ */
 public class FilterEmployeeByAge implements MyPredicate<Employee> {
 
 	@Override
