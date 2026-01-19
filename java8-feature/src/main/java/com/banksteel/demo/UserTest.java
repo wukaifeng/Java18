9fcda1acca61090aa17df53b45f862a42527625b
wukaifeng <wukf@banksteel.com>
2026-01-19 20:35:22 +0800

    再添加一行

--- java8-feature/src/main/java/com/banksteel/demo/UserTest.java
+++ java8-feature/src/main/java/com/banksteel/demo/UserTest.java
@@ -4,5 +4,6 @@ public class UserTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
