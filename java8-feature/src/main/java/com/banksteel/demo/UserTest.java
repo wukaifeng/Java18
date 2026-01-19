967ac098cbc863448ece4d698e5393fc1a70ba56
wukaifeng <wukf@banksteel.com>
2026-01-19 20:33:02 +0800

    添加一行

--- java8-feature/src/main/java/com/banksteel/demo/UserTest.java
+++ java8-feature/src/main/java/com/banksteel/demo/UserTest.java
@@ -3,5 +3,6 @@ package com.banksteel.demo;
 public class UserTest {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("Hello World!");
     }
 }
